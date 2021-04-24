2f96c0e37047e2b1ddfc7d6133ada72698ebcada
Flavio Lopes <flaviotxa@gmail.com>
2021-04-24 12:50:42 +0100

    Added side effect to scrolling component

--- pages/imageChangeOnScroll.js
+++ pages/imageChangeOnScroll.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageToggleOnScroll from "../src/imageToggleOnScroll";
 
 const ImageChangeOnScroll = () => {
+  const [currentSpeakerId, setCurrentSpeakerId] = useState(0);
+  const [mouseEventCnt, setMouseEventCnt] = useState(0);
+
+  useEffect(() => {
+    window.document.title = `SpeakerId: ${currentSpeakerId}`;
+    console.log(`useEffect: setting title to ${currentSpeakerId}`);
+  }, [currentSpeakerId]);
+
   return (
     <div>
+      <span>{`mouseEventCnt: ${mouseEventCnt}`}</span>
       {[1124, 187, 1269, 1530].map((speakerId) => {
         return (
-          <div ket={speakerId}>
+          <div
+            ket={speakerId}
+            onMouseOver={() => {
+              setCurrentSpeakerId(speakerId);
+              setMouseEventCnt(mouseEventCnt + 1);
+              console.log(`onMouseOver:${speakerId}`);
+            }}
+          >
             <ImageToggleOnScroll
               primaryImg={`static/speakers/bw/Speaker-${speakerId}.jpg`}
               secondaryImg={`static/speakers/Speaker-${speakerId}.jpg`}
